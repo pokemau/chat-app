705516b6da1379b02f70fa31c669d6e2a37515a7
pokemau <rentillosa90@gmail.com>
2022-12-10 10:01:08 +0800

    limit message query to 1

--- components/chatroom/Messages.tsx
+++ components/chatroom/Messages.tsx
@@ -1,11 +1,17 @@
-import { collection, DocumentData, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  DocumentData,
+  limit,
+  orderBy,
+  query,
+} from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../../firebase/client";
 import Image from "next/image";
 
 const Messages = () => {
   const [value, loading, error] = useCollection(
-    query(collection(db, "chats"), orderBy("timeStamp"))
+    query(collection(db, "chats"), orderBy("timeStamp"), limit(1))
   );
 
   const chats = value?.docs.map((doc) => ({ id: doc.id, data: doc.data() }));
@@ -20,14 +26,14 @@ const Messages = () => {
         {chats?.map((chat) => (
           <div key={chat.id} className="relative px-2 mb-2 break-all">
             <Image
-              className="rounded-full mr-2 absolute left-0"
+              className="rounded-full mr-2 absolute left-0 top-1"
               src={chat.data.userImg}
-              width={45}
-              height={45}
+              width={40}
+              height={40}
               alt="nig"
             />
 
-            <div className="ml-12">
+            <div className="ml-10">
               <div className="flex">
                 <h1 className="font-bold">{chat.data.userName}</h1>
                 <span className="ml-2 text-[#adadad]">
