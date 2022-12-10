13c20e4e5fbdd51b67254b0ae320dac5f7e92245
pokemau <rentillosa90@gmail.com>
2022-12-10 11:39:51 +0800

    limit messages on page load to 15 and can load more

--- components/chatroom/Messages.tsx
+++ components/chatroom/Messages.tsx
@@ -1,14 +1,26 @@
 import { collection, limit, orderBy, query } from "firebase/firestore";
+import { MouseEvent, useState } from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "../../firebase/client";
 import Image from "next/image";
 
 const Messages = () => {
+  const [limitVal, setLimitVal] = useState(15);
   const [value, loading, error] = useCollection(
-    query(collection(db, "chats"), orderBy("timeStamp", "desc"), limit(15))
+    query(
+      collection(db, "chats"),
+      orderBy("timeStamp", "desc"),
+      limit(limitVal)
+    )
   );
 
   const chats = value?.docs.map((doc) => ({ id: doc.id, data: doc.data() }));
+  chats?.reverse();
+
+  const getMoreMessages = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setLimitVal(limitVal + 5);
+  };
 
   // userMessage
   // userName
@@ -18,7 +30,7 @@ const Messages = () => {
     return (
       <>
         {chats?.map((chat) => (
-          <div key={chat.id} className="relative px-2 mb-2 break-all">
+          <div key={chat.id} className="relative px-2 mb-2 break-all ">
             <Image
               className="rounded-full mr-2 absolute left-0 top-1"
               src={chat.data.userImg}
@@ -45,6 +57,12 @@ const Messages = () => {
 
   return (
     <div className="py-2 sidebar h-[75%] overflow-auto border-b-[1px]">
+      <button
+        className="underline mb-2 hover:text-[#165ac2]"
+        onClick={getMoreMessages}>
+        Show More Messages
+      </button>
+
       <Chat />
     </div>
   );
