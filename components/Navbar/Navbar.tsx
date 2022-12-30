1bce82664c21c676f9b9393edcf8831d640e4354
pokemau <rentillosa90@gmail.com>
2022-12-30 16:51:50 +0800

    allow go home on logo

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,7 @@
 import HamburgerMenu from "../menu/HamburgerMenu";
 
+import Link from "next/link";
+
 // utils
 import { useTheme } from "next-themes";
 // react icons
@@ -28,7 +30,10 @@ const Navbar = () => {
   };
   return (
     <nav className="flex w-full h-[10vh] px-6 md:px-20 items-center border-b-[1px] border-[#ddd] dark:border-[#0c0f1d] dark:bg-[#1B1D2A] ">
-      <h1 className="font-bold text-2xl">Yahallo</h1>
+      <Link className="font-bold text-2xl" href="/">
+        Yahallo
+      </Link>
+      {/* <h1 className="font-bold text-2xl">Yahallo</h1> */}
 
       <div className="flex ml-auto items-center gap-2">
         <div className="cursor-pointer mr-2">{themeChanger()}</div>
