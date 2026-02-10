0b38df72f8deac66ee4ac9d68642b2ec773624f7
pokemau <rentillosa90@gmail.com>
2026-02-10 08:54:21 +0800

    env

--- src/firebase/client.tsx
+++ src/firebase/client.tsx
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOEMDWW-pQf6nghWBThm_y_PpAtfCBmhk",
-  authDomain: "rc-bot-2b830.firebaseapp.com",
-  projectId: "rc-bot-2b830",
-  storageBucket: "rc-bot-2b830.appspot.com",
-  messagingSenderId: "580067608589",
-  appId: "1:580067608589:web:8b11140ff124d0885a6d44",
-  measurementId: "G-0ZEGZXMK5F",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
